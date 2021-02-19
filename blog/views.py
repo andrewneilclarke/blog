812cdbd1694f305b676e10c394817dac883f0718
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-19 16:36:17 +0100

    created register form

--- blog/views.py
+++ blog/views.py
@@ -1,32 +1,15 @@
 from django.shortcuts import render
-
-posts = [
-		{
-		'author' : 'Andrew C',
-		'title': 'how to learn python',
-		'content': 'first lessons content',
-		'date_posted': 'August 24, 2020'
-
-		}
-
-		{
-		'author' : 'Chris C',
-		'title': 'how to learn html',
-		'content': 'first lessons content',
-		'date_posted': 'August 25, 2020'
-
-		}
-]
+from . models import Post
 
 # Create your views here.
 def home(request):
 	context = {
-		'posts': posts
+		'posts': Post.objects.all()
 		}
 	return render(request, 'blog/home.html', context)
 
 def about(request):
-	return render(request, 'blog/about.html')
+	return render(request, 'blog/about.html', {'title': 'About'})
 
 
  
