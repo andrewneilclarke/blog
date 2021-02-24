437efc33c733e1cadd0d3433647b539c8d8d68fa
Andrew Clarke <andrewneilclarke@gmail.com>
2021-02-24 17:59:55 +0100

    more functionality added

--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,5 @@
 from django.shortcuts import render
+from django.views.generic import ListView, DetailView, CreateView
 from . models import Post
 
 # Create your views here.
@@ -7,6 +8,19 @@ def home(request):
 		'posts': Post.objects.all()
 		}
 	return render(request, 'blog/home.html', context)
+
+class PostListView(ListView):
+	model = Post
+	template_name = 'blog/home.html'
+	context_object_name = 'posts'
+	ordering = ['-date_posted']
+
+class PostDetailView(DetailView):
+	model = Post
+
+class PostCreateView(CreateView):
+	model = Post
+	fields = ['title', 'content']
 
 def about(request):
 	return render(request, 'blog/about.html', {'title': 'About'})
