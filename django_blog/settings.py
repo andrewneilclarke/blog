1ad82e3f7a035d14524a3d4e17fc8f6f91af5077
Andrew Clarke <andrewneilclarke@gmail.com>
2021-03-02 11:31:42 +0100

    set debug based on env. variables

--- django_blog/settings.py
+++ django_blog/settings.py
@@ -143,5 +143,5 @@ django_heroku.settings(locals())
 
 if os.environ.get('DEBUG') == 'TRUE':
     DEBUG = True
-elif: os.environ.get('DEBUG') == 'FALSE':
+elif os.environ.get('DEBUG') == 'FALSE':
     DEBUG = False
